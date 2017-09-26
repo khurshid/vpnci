4ed3faae7d064c0cd3bc1f9c3f9be34b46432154
khurshid alam <khurshid.alam@zingmobile.com.sg>
2017-09-26 13:35:26 +0530

    formated files

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,43 +1,45 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Login extends CI_Controller {
-
-	public function index($e=false)
-	{
-		//$this->output->enable_profiler(true);
-		$data['pageTitle'] = 'Zingmobile-VPN Login';
-		$data['error']='';
-		if($e) $data['error'] = 'Inavlid E-mail and password combination!';
-		$this->load->view('l_header',$data);
-		$this->load->view('login');
-		$this->load->view('l_footer');
-        	}
-	public function auth()
-	{
-		$this->load->model('model_login');
-		$email=$this->input->post('email');
-		$password=$this->input->post('password');
-	    $row=$this->model_login->auth($email,$password);
-		$this->session->set_userdata('profile', $row);
-		if($row['id']) redirect('/users','location', 301);
-		else redirect('/login/index/1','location', 301);
-	}
-	public function forgotpassword()
-	{
-	  $email=$this->input->post('email');
-	  if(!empty($email))
-	  {
-     return 1;
-	  }
-
-	}
-	/* Logout from the appplication */
-	public function logout()
-	{
-		$this->session->sess_destroy();
-		redirect('/login','location', 301);
-		exit;
-
-	}
+class Login extends CI_Controller
+{
+    public function index($e=false)
+    {
+        //$this->output->enable_profiler(true);
+        $data['pageTitle'] = 'Zingmobile-VPN Login';
+        $data['error']='';
+        if ($e) {
+            $data['error'] = 'Inavlid E-mail and password combination!';
+        }
+        $this->load->view('l_header', $data);
+        $this->load->view('login');
+        $this->load->view('l_footer');
+    }
+    public function auth()
+    {
+        $this->load->model('model_login');
+        $email=$this->input->post('email');
+        $password=$this->input->post('password');
+        $row=$this->model_login->auth($email, $password);
+        $this->session->set_userdata('profile', $row);
+        if ($row['id']) {
+            redirect('/users', 'location', 301);
+        } else {
+            redirect('/login/index/1', 'location', 301);
+        }
+    }
+    public function forgotpassword()
+    {
+        $email=$this->input->post('email');
+        if (!empty($email)) {
+            return 1;
+        }
+    }
+    /* Logout from the appplication */
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        redirect('/login', 'location', 301);
+        exit;
+    }
 }
